f13b0fa5a0921b304cc50f25f009da5481ef006c
sumaya257 <sumayaece19@gmai.com>
2024-12-06 15:09:11 +0600

    posting-a-movie-to-the-database

--- index.js
+++ index.js
@@ -27,12 +27,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //server to database
+    const movieCollection = client.db('movieDB').collection('movie')
+    //after creating now read the data
+    
+    //client to server
+    app.post('/movie',async(req,res)=>{
+        const newMovie = req.body
+        console.log(newMovie)
+        const result = await movieCollection.insertOne(newMovie)
+        res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
